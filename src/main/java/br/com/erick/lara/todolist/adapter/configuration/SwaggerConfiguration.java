dbff894f707365d2d152f1bf836ceeb9cf2217d5
Erick Lara <egmlara81@gmail.com>
2022-11-15 23:05:33 -0300

    feat: Swagger and configurations

---
+++ src/main/java/br/com/erick/lara/todolist/adapter/configuration/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package br.com.erick.lara.todolist.adapter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.erick.lara.todolist.adapter.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("to-do-list-backend")
+                .description("Rest API made for the backend funcionalities of a To Do List, just for tests.")
+                .version("1.0.0")
+                .contact(new Contact("Erick Lara", "https://github.com/erick-lara", "egmlara81@gmail.com"))
+                .build();
+    }
+}
--- src/main/java/br/com/erick/lara/todolist/adapter/controller/TaskController.java
+++ src/main/java/br/com/erick/lara/todolist/adapter/controller/TaskController.java
@@ -3,6 +3,10 @@ package br.com.erick.lara.todolist.adapter.controller;
 import br.com.erick.lara.todolist.adapter.controller.dto.request.TaskRequest;
 import br.com.erick.lara.todolist.domain.Task;
 import br.com.erick.lara.todolist.usecase.TaskService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,41 +20,72 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @ApiOperation(value = "Cria uma nova task.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
     @PostMapping
     public ResponseEntity<Void> createTask(@RequestBody TaskRequest request){
         taskService.createTask(request);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Busca todas as tasks registradas.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
     @GetMapping
     public ResponseEntity<List<Task>> findAll(){
         return ResponseEntity.ok(taskService.findAllTasks());
     }
 
+    @ApiOperation(value = "Busca a task por um ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Task> findTaskById(@PathVariable int id){
+    public ResponseEntity<Task> findTaskById(@ApiParam @PathVariable int id){
         return ResponseEntity.ok(taskService.findTaskById(id));
     }
 
+    @ApiOperation(value = "Atualiza a task por um ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateTask(@PathVariable int id, @RequestBody TaskRequest request){
+    public ResponseEntity<Void> updateTask(
+            @ApiParam @PathVariable int id,
+            @ApiParam @RequestBody TaskRequest request
+    ){
         taskService.updateTask(id, request);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Marca uma task como completa por seu ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
     @PutMapping("/complete/{id}")
-    public ResponseEntity<Void> markTaskComplete(@PathVariable int id){
+    public ResponseEntity<Void> markTaskComplete(@ApiParam @PathVariable int id){
         taskService.markTaskCompleted(id);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Busca todas as tasks ativas.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
     @GetMapping("/active")
     public ResponseEntity<List<Task>> findAllActiveTasks(){
         return ResponseEntity.ok(taskService.findAllActiveTasks());
     }
 
+    @ApiOperation(value = "Exclui uma task por seu ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable int id){
+    public ResponseEntity<Void> deleteTask(@ApiParam @PathVariable int id){
         taskService.deleteTask(id);
         return ResponseEntity.ok().build();
     }
